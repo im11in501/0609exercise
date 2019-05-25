71f62e1894dbb3717391ad5c5b40cef2e0981625
1052988 <s1052988@mail.ncyu.edu.tw>
2019-05-25 15:58:50 +0800

    0609exercise_1052988_04

--- deep_nested_if/SCM.java
+++ deep_nested_if/SCM.java
@@ -10,45 +10,51 @@ public class SourceControlManager
 
     public boolean checkin(SCMServer scmServer, CICDServer cicdserver, String userName, String passwd, String commitMsg, Change[] changes)
     {
-        if (scmServer != null && scmServer.isConnected())
+        if (scmServer == null || !scmServer.isConnected())
         {
-            if (cicdServer != null && cicdServer.isReady())
+			system.out.println("scm server problem.");
+            return 6;
+        }
+		else {
+            if (cicdServer == null || !cicdServer.isReady())
             {
-                if (userName != null)
+				system.out.println("cicd server problem.");
+                return 5;
+            } 
+			else {
+                if (userName == null)
                 {
-                    if (passd != null && passwd.length() > 15)
+					system.out.println("Invalid user name.");
+                    return 4;
+                } 
+				else {
+                    if (passd == null || passwd.length() <= 15)
                     {
-                        if (commitMsg != null && commitMsg < 200)
+						system.out.println("Invalid passwd.");
+                        return 3;
+                    } 
+					else {
+                        if (commitMsg == null || commitMsg >= 200)
                         {
-                            if (changes != null && changes.length() > 0)
+							System.out.println("Invalid Commit message.");
+                            return 2;
+                        } 
+						else {
+                            if (changes == null || changes.length() <= 0)
                             {
+								System.out.println("No change.");
+                                return 1;
+                            } 
+							else {
                                 System.out.println("Ready to push to SCM server.");
                                 scmServer.push(userName, passwd, commitMsg, changes);
                                 System.out.println("Finish to push to SCM server.");
                                 return 0;
-                            } else {
-                                System.out.println("No change.");
-                                return 1;
                             }
-                        } else {
-                            System.out.println("Invalid Commit message.");
-                            return 2;
                         }
-                    } else {
-                        system.out.println("Invalid passwd.");
-                        return 3;
                     }
-                } else {
-                    system.out.println("Invalid user name.");
-                    return 4;
                 }
-            } else {
-                system.out.println("cicd server problem.");
-                return 5;
             }
-        } else {
-            system.out.println("scm server problem.");
-            return 6;
         }
     }
 }
